878fca6a10d51b065b10f674e3671faeb914e1d7
sallysway <ioanaandreea476@gmail.com>
2016-03-04 13:21:29 +0200

    first commit

---
+++ Administration/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration
+{
+    class Book
+    {
+        public int Index { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+        public string Language { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+        List<Book> AllBooks = new List<Book>();
+
+        public void AddBook()
+        {
+            Console.WriteLine("Please insert details of the book");
+            Console.WriteLine("Title: ");
+
+            Book thisBook = new Book();
+        }
+
+        public void SearchBook()
+        {
+            Console.WriteLine("Give me the title please\n");
+            var searchedtitle = Console.ReadLine().ToString();
+            foreach (var book in AllBooks)
+            {
+                if (book.Title == searchedtitle)
+                {
+                    Console.WriteLine("yeap, found it!\n");
+                    book.ShowBook();
+                }
+            }
+        }
+
+        private void ShowBook()
+        {
+
+        }
+    }
+
+}
+
---
+++ Administration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book m = new Book();
+            m.AddBook();
+
+        }
+    }
+}
